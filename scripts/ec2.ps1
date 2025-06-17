520e938b7a372487a921f813bc182579d308d558
THANNIRU SANKAR GANESH <thanniru.sanka@hcl.com>
2025-06-17 18:48:10 +0530

    07

--- scripts/ec2.ps1
+++ scripts/ec2.ps1
@@ -34,7 +34,7 @@ try
      }
  
      $dataRow = [pscustomobject]@{
-            #"Account Name"      = $script:AccountName
+            "Account Name"      = $script:AccountName
             "Region" = $script:Region
             "Name"=($Instance.Tag | Where-Object -FilterScript {$_.Key -eq 'Name'}).Value
             "InstanceID" = $InstanceID
@@ -67,7 +67,7 @@ $Script:EC2Data=@()
 Write-Output "$(get-date -Format "dd_MM_yyyy_hh_mm_ss") - Script Starts"
 write-output "Fetching Accounts"
 $script:Accounts = '222634374835'
-#$script:AccountName = 'AWS-NN-ConcurExpense-TST'
+$script:AccountName = 'AWS-Eops'
 $script:Regions = @("us-east-2","eu-west-1")
  
  
