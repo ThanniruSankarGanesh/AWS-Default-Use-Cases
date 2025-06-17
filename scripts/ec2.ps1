4fe27b5457162a169155922402fe7687a5846278
THANNIRU SANKAR GANESH <thanniru.sanka@hcl.com>
2025-06-17 17:02:04 +0530

    files added8

--- scripts/ec2.ps1
+++ scripts/ec2.ps1
@@ -1,5 +1,5 @@
-# Install-Module -Name "AWS.Tools.EC2" -Force -scope CurrentUser
-# Import-Module -Name "AWS.Tools.EC2"
+Install-Module -Name "AWS.Tools.Common" -Force -scope CurrentUser
+ Import-Module -Name "AWS.Tools.Common" -Force
 
 Function EC2
 {
