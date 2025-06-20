4f65e31a442f5808f9cf941bb580b5ad26d5b38e
THANNIRU SANKAR GANESH <thanniru.sanka@hcl.com>
2025-06-20 10:39:03 +0530

    files 12

--- scripts/s3-report.ps1
+++ scripts/s3-report.ps1
@@ -176,7 +176,7 @@ write-output "$(get-date -Format "dd_MM_yyyy_hh_mm_ss") - Script Starts"
 write-output "Fetching Accounts"
 $script:Accounts = @('222634374835')
 $script:AccountName = 'AWS-Eops'
-$script:Regions = @("us-east-2","eu-west-1")
+$script:Regions = @("us-east-1")
     
        S3Reporting
 
